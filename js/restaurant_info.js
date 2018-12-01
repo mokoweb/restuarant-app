15e4e710d51441b5d9a1914398d94f4ccb1c7a6b
Amoko Olaolu Daniel <mokowebtech@gmail.com>
2018-12-02 00:38:38 +0100

    Update restaurant_info.js

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -116,8 +116,6 @@ const favBtn = document.createElement('button');
       DBHelper.setFavorite(restaurant.id);
     }
     favBtn.classList.toggle('active');
- 
-  li.append(favBtn);
   });
 
   const image = document.getElementById('restaurant-img');
@@ -278,4 +276,181 @@ const toggleModal = (evt) => {
   evt.preventDefault();
   const modal = document.getElementById('modal');
   modal.classList.toggle('show');
+};
+
+var focusedElementBeforeModal;
+const modal = document.getElementById('modal');
+const modalOverlay = document.querySelector('.modal-overlay');
+
+// Adapted from modal dialog sample code in Udacity Web Accessibility course 891
+const openModal = () => {
+  // Save current focus
+  focusedElementBeforeModal = document.activeElement;
+
+  // Listen for and trap the keyboard
+  modal.addEventListener('keydown', trapTabKey);
+
+  // Listen for indicators to close the modal
+  modalOverlay.addEventListener('click', closeModal);
+  // Close btn
+  const closeBtn = document.querySelector('.close-btn');
+  closeBtn.addEventListener('click', closeModal);
+
+  // submit form
+  const form = document.getElementById('review-form');
+  form.addEventListener('submit', saveAddReview, false);
+
+  // Find all focusable children
+  var focusableElementsString = 'a[href], area[href], input:not([disabled]),' +
+    'select:not([disabled]), textarea:not([disabled]), button:not([disabled]),' + 
+    'iframe, object, embed, [tabindex="0"], [contenteditable]';
+  var focusableElements = modal.querySelectorAll(focusableElementsString);
+  // Convert NodeList to Array
+  focusableElements = Array.prototype.slice.call(focusableElements);
+
+  var firstTabStop = focusableElements[0];
+  var lastTabStop = focusableElements[focusableElements.length - 1];
+
+  // Show the modal and overlay
+  modal.classList.add('show');
+  modalOverlay.classList.add('show');
+
+  // Focus first child
+  // firstTabStop.focus();
+  const reviewName = document.getElementById('reviewName');
+  reviewName.focus();
+
+  function trapTabKey(e) {
+    // Check for TAB key press
+    if (e.keyCode === 9) {
+
+      // SHIFT + TAB
+      if (e.shiftKey) {
+        if (document.activeElement === firstTabStop) {
+          e.preventDefault();
+          lastTabStop.focus();
+        }
+
+      // TAB
+      } else {
+        if (document.activeElement === lastTabStop) {
+          e.preventDefault();
+          firstTabStop.focus();
+        }
+      }
+    }
+
+    // ESCAPE
+    if (e.keyCode === 27) {
+      closeModal();
+    }
+  }
+};
+
+const saveAddReview = (e) => {
+  e.preventDefault();
+
+  const name = document.querySelector('#reviewName').value;
+  const rating = document.querySelector('input[name=rate]:checked').value;
+  const comments = document.querySelector('#reviewComments').value;
+  
+  // console.log(name);
+  // console.log(rating);
+  // console.log(comments);
+
+  DBHelper.createRestaurantReview(self.restaurant.id, name, rating, comments,
+    (error, review) => {
+    console.log('got callback');
+    if (error) {
+      console.log('Error saving review');
+    } else {
+      // do some other stuff
+      console.log(review);
+      window.location.href = `/restaurant.html?id=${self.restaurant.id}`;
+    }
+  });
+};
+
+const closeModal = () => {
+  // Hide the modal and overlay
+  modal.classList.remove('show');
+  modalOverlay.classList.remove('show');
+
+  const form = document.getElementById('review-form');
+  form.reset();
+  // Set focus back to element that had it before the modal was opened
+  focusedElementBeforeModal.focus();
+};
+
+const setFocus = (evt) => {
+  const rateRadios = document.getElementsByName('rate');
+  const rateRadiosArr = Array.from(rateRadios);
+  const anyChecked = rateRadiosArr.some(radio => { 
+    return radio.checked === true; 
+  });
+  if (!anyChecked) {
+    const star1 = document.getElementById('star1');
+    star1.focus();
+  }
+};
+
+// this code is done for proper a11y & keyboard nav
+const navRadioGroup = (evt) => {
+  const star1 = document.getElementById('star1');  
+  const star2 = document.getElementById('star2');  
+  const star3 = document.getElementById('star3');  
+  const star4 = document.getElementById('star4');  
+  const star5 = document.getElementById('star5');  
+
+  if (['ArrowRight', 'ArrowLeft', 'ArrowDown', 'ArrowUp'].includes(evt.key)) {
+    evt.preventDefault();
+    // console.log('attempting return');
+    if (evt.key === 'ArrowRight' || evt.key === 'ArrowDown') {
+      switch(evt.target.id) {
+        case 'star1':
+          star2.focus();
+          star2.checked = true;
+          break;
+        case 'star2':
+          star3.focus();
+          star3.checked = true;
+          break;
+        case 'star3':
+          star4.focus();
+          star4.checked = true;
+          break;
+        case 'star4':
+          star5.focus();
+          star5.checked = true;
+          break;
+        case 'star5':
+          star1.focus();
+          star1.checked = true;
+          break;
+      }
+    } else if (evt.key === 'ArrowLeft' || evt.key === 'ArrowUp') {
+      switch(evt.target.id) {
+        case 'star1':
+          star5.focus();
+          star5.checked = true;
+          break;
+        case 'star2':
+          star1.focus();
+          star1.checked = true;
+          break;
+        case 'star3':
+          star2.focus();
+          star2.checked = true;
+          break;
+        case 'star4':
+          star3.focus();
+          star3.checked = true;
+          break;
+        case 'star5':
+          star4.focus();
+          star4.checked = true;
+          break;
+      }
+    }
+  }
 };
